5cb4c325fe0a585c7716ed070ab6b94362681a50
wamuir <17497234+wamuir@users.noreply.github.com>
2020-06-06 20:12:38 -0700

    set content type for health checks

--- main.go
+++ main.go
@@ -238,30 +238,14 @@ func handleError(w http.ResponseWriter, code int) {
 // Endpoint for health checks by haproxy, etc.
 func health(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusOK)
 	encoder := json.NewEncoder(w)
 	encoder.SetIndent("", "\t")
 	encoder.Encode(map[string]bool{"ok": true})
 	return
-}
-
-// Log to stdout
-func logging(next http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				stdout.Println(
-					r.Method,
-					r.Proto,
-					r.URL.Path,
-					r.RemoteAddr,
-					r.UserAgent(),
-				)
-			}()
-			next.ServeHTTP(w, r)
-			return
-		},
-	)
 }
 
 func main() {
